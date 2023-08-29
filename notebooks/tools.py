5589abc8ffa7fbc4818e02857dc46a3e1d23523c
Karthik Venkataramani <karthikv@jpl.nasa.gov>
2023-08-29 15:17:40 -0700

    adding comments

--- notebooks/tools.py
+++ notebooks/tools.py
@@ -17,25 +17,23 @@ from skimage.restoration import denoise_tv_bregman
 import numpy as np
 
 # misc imports
-from typing import Tuple, List
+from typing import List
 from pathlib import Path, PosixPath
 from itertools import product
 import os
-from collections import defaultdict
 
-def retrieve_hansen_mosaic(bounds, data_product='last', download_path=Path('./')) -> List[PosixPath]:
-    '''
-    Given a raster bounds, retrieve the Hansen Landsat mosaic that covers this tile. If the span of the
-    provided image extends beyond a single tile, multiple tiles will be downloaded 
+def retrieve_hansen_mosaic(bounds:list, data_product:str='last', download_path:PosixPath=Path('./')) -> List[PosixPath]:
+    """Given a raster bounds, retrieve the Hansen Landsat mosaic that covers this tile. If the span of the
+    provided image extends beyond a single tile, multiple tiles will be downloaded.
 
-    Arguments:
-    ----------
-    bounds : rasterio bounding box describing span of the raster in degrees (left, bottom, right, top)
-    data_product : default 'last'. Can be one of the Hansen GFC products (treecover, gain, lossyear, datamask, first, last)
+    Args:
+        bounds (list): rasterio bounding box describing span of the raster in degrees (left, bottom, right, top)
+        data_product (str, optional): Data product to download. Defaults to 'last'. Can be one of the Hansen GFC products (treecover, gain, lossyear, datamask, first, last)
+        download_path (PosixPath, optional): Download location for data products. Defaults to Path('./').
 
-    Returns : a list of paths to the downloaded file(s)
-    '''
-
+    Returns:
+        List[PosixPath]: a list of paths to the downloaded file(s)
+    """
     assert min(bounds) >=-180, "Bounds must be specified in EPSG:4236"
     assert min(bounds) <= 360, "Bounds must be specified in EPSG:4236"
 
@@ -63,7 +61,6 @@ def retrieve_hansen_mosaic(bounds, data_product='last', download_path=Path('./')
     nx_ny = list(product(x_tiles, y_tiles))
 
     def download_one_tile(edges, data_product, download_path=Path('./')):
-        
         left_edge, top_edge = edges
         wget_command = 'https://storage.googleapis.com/earthenginepartners-hansen/GFC-2020-v1.8/Hansen_GFC-2020-v1.8_{}_{}_{}.tif'
 
@@ -82,7 +79,18 @@ def retrieve_hansen_mosaic(bounds, data_product='last', download_path=Path('./')
     outputs = list(map(download_one_tile, nx_ny, [data_product]*len(nx_ny), [download_path]*len(nx_ny)))
     return outputs
 
-def denoise(img, weight=0.2, input_db=False, return_db=False):
+def denoise(img:np.ndarray, weight=0.2, input_db=False, return_db=False) -> np.ndarray :
+    """Apply TV denoising to input SAR image
+
+    Args:
+        img (np.ndarray): image to be denoised
+        weight (float, optional): Weight parameter for denoising. Lower values result in greater denoising. Defaults to 0.2.
+        input_db (bool, optional): Set to True if input image is in logarithmic units. Defaults to False.
+        return_db (bool, optional): Set to True if returned image should also be in logarithmic units. Defaults to False.
+
+    Returns:
+        np.ndarray: Denoised image
+    """
 
     idx = np.where((np.isnan(img)) | (np.isinf(img)) | (img == 0))
     
