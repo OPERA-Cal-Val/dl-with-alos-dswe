70b86fd652bc529b67b49150fd3574b9748c88a0
Karthik Venkataramani <karthikv@jpl.nasa.gov>
2023-08-29 15:18:17 -0700

    dataloader methods

--- notebooks/dataloader.py
+++ notebooks/dataloader.py
@@ -11,8 +11,27 @@ import numpy as np
 from tools import denoise
 import random
 
+# ML imports
+import torch
+import segmentation_models_pytorch as smp
+import pytorch_lightning as pl
+# from pytorch_lightning.strategies import DDPStrategy
+
+# gis imports
+import rasterio
+
+# math/pandas/imtools imports
+import numpy as np
+
+# misc imports
+import datetime
+from pathlib import Path
+from tools import denoise
+import random
+
 class sarDataLoader(torch.utils.data.Dataset):
-    def __init__(self, x_paths, y_paths, transforms=None, return_sar=True, return_optical=True, return_dem=True, channel_drop=False):
+    def __init__(self, x_paths, y_paths, transforms=None, return_sar=True, denoising_weight = 0.2, 
+    return_optical=True, return_dem=True, return_hand=True, channel_drop=False):
         super().__init__()
         self.data = x_paths
         self.label = y_paths
@@ -20,7 +39,9 @@ class sarDataLoader(torch.utils.data.Dataset):
         self.return_sar = return_sar
         self.return_optical = return_optical
         self.return_dem = return_dem
+        self.return_hand = return_hand
         self.channel_drop = channel_drop
+        self.denoising_weight = denoising_weight
         
     def __len__(self):
         return len(self.data)
@@ -34,28 +55,32 @@ class sarDataLoader(torch.utils.data.Dataset):
         data_chips = []
         if self.return_sar:
             with rasterio.open(img.hh) as ds:
-                data_chips.append(denoise(np.clip(ds.read(1), 0, 1), weight=0.2, return_db=True) - self.hh_mean)
+                data_chips.append(denoise(np.clip(ds.read(1), 0, 1), weight=self.denoising_weight, return_db=True))
             with rasterio.open(img.hv) as ds:
-                data_chips.append(denoise(np.clip(ds.read(1), 0, 1), weight=0.2, return_db=True) - self.hv_mean)
+                data_chips.append(denoise(np.clip(ds.read(1), 0, 1), weight=self.denoising_weight, return_db=True))
 
         if self.return_optical:
             with rasterio.open(img.red) as ds:
-                data_chips.append(ds.read(1) - self.red_mean)
+                data_chips.append(ds.read(1))
 
             with rasterio.open(img.nir) as ds:
-                data_chips.append(ds.read(1) - self.nir_mean)
+                data_chips.append(ds.read(1))
 
             with rasterio.open(img.swir1) as ds:
-                data_chips.append(ds.read(1) - self.swir1_mean)
+                data_chips.append(ds.read(1))
 
             with rasterio.open(img.swir2) as ds:
-                data_chips.append(ds.read(1) - self.swir2_mean)
+                data_chips.append(ds.read(1))
 
         if self.return_dem:
             with rasterio.open(img.dem) as ds:
                 data_chips.append(ds.read(1))
 
-        x_arr = np.stack(data_chips, axis=-1).astype(np.float32)
+        if self.return_hand:
+            with rasterio.open(img.hand) as ds:
+                data_chips.append(ds.read(1))
+
+        x_arr = np.stack(data_chips, axis = -1).astype(np.float32)
         
         if (self.return_optical is True) and (self.channel_drop is True) and (random.random() > 0.2):
             for c in random.choices([2, 3, 4, 5], k=3):
